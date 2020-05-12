fec80d622e7635e09e6317609766898ba8d0c472
Bryan.Yen <bryan.yen@yfyshop.com>
2020-05-12 14:08:53 +0800

    # 首頁加參數

--- index.php
+++ index.php
@@ -90,3 +90,62 @@
 <!-- ============= END main ============= -->
 <?php include_once 'layouts/footer.php';?>
 
+<script>
+    let actionUrl = 'includes/services.php';
+    let server = "https://isearch.liveplates.com/quick_payment/";
+
+    var urlParams = new URLSearchParams(window.location.search);
+
+    if (urlParams.has('community_id')) {
+        community_id = urlParams.get('community_id');
+    } else {
+        community_id = null;
+    }
+
+    if (urlParams.has('user_id')) {
+        user_id = urlParams.get('user_id');
+    }else {
+        user_id = null;
+    }
+
+    $(document).ready(function () {
+        if (community_id == null && user_id == null) {
+            community_id = 1100580014;
+            user_id = 105382;
+            //測試模式
+            // alert("參數不足");
+        }
+
+        // init(community, communityNmae, user, account, address);
+
+    });
+
+   // function init(community, communityNmae, user, account, address) {
+   //      let searchParams = new URLSearchParams({
+   //        community: community,
+   //        community_name: communityNmae,
+   //        user: user,
+   //        account: account
+   //      });
+
+   //      $('#paymentList').click(function() {
+   //          const listURL = new URL(server+'list.php');
+   //          listURL.search = searchParams;
+
+   //          window.location.href= listURL.href; //需要跳轉的地址
+   //      });
+   //      $('#invoiceList').click(function() {
+   //          const invoiceURL = new URL(server+'invoice.php');
+   //          invoiceURL.search = searchParams;
+   //          window.location.href= invoiceURL.href; //需要跳轉的地址
+   //      });
+
+   //      $('#account').text(account);
+   //      $('#community-name').text(communityNmae);
+   //      $('#community-address').text(address);
+   //  }
+
+</script>
+
+</body>
+</html>
